04ad3e264bbe5246c51d702b42e74d82f785febb
bgorven <bgor010@aucklanduni.ac.nz>
2014-10-21 05:10:57 +1300

    Connected NewSiteButton to a command

--- Discouser/Discouser.Shared/Command.cs
+++ Discouser/Discouser.Shared/Command.cs
@@ -5,19 +5,84 @@ using System.Windows.Input;
 
 namespace Discouser
 {
-    class Command : ICommand
+    class Command<TParameter> : ICommand
     {
+        /// <summary>
+        /// Creates a command that executes if the parameter is of the correct type
+        /// </summary>
+        public Command(Action<TParameter> action) : this(anything => true, action) { }
+
+        /// <summary>
+        /// Creates a command that executes if the parameter is of the correct type and the predicate returns true
+        /// </summary>
+        public Command(Func<TParameter, bool> predicate, Action<TParameter> action)
+        {
+            _predicate = predicate;
+            _action = action;
+        }
+
+        private Action<TParameter> _action;
+        private Func<TParameter, bool> _predicate;
 
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return parameter is TParameter && _predicate((TParameter)parameter);
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            if (parameter is TParameter)
+            {
+                _action((TParameter)parameter);
+            }
+        }
+    }
+
+    class Command : ICommand
+    {
+        /// <summary>
+        /// Creates a command that executes if the parameter is of the correct type
+        /// </summary>
+        public Command(Action action) : this(() => true, action) { }
+
+        /// <summary>
+        /// Creates a command that executes if the parameter is of the correct type and the predicate returns true
+        /// </summary>
+        public Command(Func<bool> predicate, Action action)
+        {
+            _predicate = predicate;
+            _action = action;
+        }
+
+        private Action _action;
+        private Func<bool> _predicate;
+
+        public event EventHandler CanExecuteChanged;
+
+        internal void Notify()
+        {
+            OnCanExecuteChanged();
+        }
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _predicate();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
         }
     }
 }
--- Discouser/Discouser.Shared/ViewModel/AllSites.cs
+++ Discouser/Discouser.Shared/ViewModel/AllSites.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 
 namespace Discouser.ViewModel
 {
@@ -27,11 +28,24 @@ namespace Discouser.ViewModel
             LocalGuid = (Guid)localSettings[guidSettingsKey];
 
             Sites = new ObservableCollection<Site>(logins.Select(l => new Site(new DataContext(l.Item1, l.Item2, (Guid)LocalGuid))));
+
+            NewSiteCommand = new Command(CanAddNewSite, AddNewSite);
         }
 
-        public void AddSite(string url, string username)
+        private bool CanAddNewSite()
         {
-            AddSite(Tuple.Create(url, username));
+            return !string.IsNullOrEmpty(NewSiteUrl)
+                && Uri.IsWellFormedUriString(NewSiteUrl.StartsWith("http", StringComparison.OrdinalIgnoreCase) ? NewSiteUrl : "http://" + NewSiteUrl, UriKind.Absolute)
+                && !string.IsNullOrEmpty(NewSiteUsername)
+                && !string.IsNullOrEmpty(NewSitePassword);
+        }
+
+        private void AddNewSite()
+        {
+            AddSite(Tuple.Create(NewSiteUrl, NewSiteUsername));
+            NewSiteUrl = "";
+            NewSiteUsername = "";
+            NewSitePassword = "";
         }
 
         private void AddSite(Tuple<string, string> login)
@@ -60,6 +74,36 @@ namespace Discouser.ViewModel
             foreach (var site in Sites)
             {
                 site.Context.Dispose();
+            }
+        }
+
+        public Command NewSiteCommand { get; private set; }
+        private string _newSiteUsername;
+        public string NewSiteUsername { get { return _newSiteUsername; }
+            set
+            {
+                NewSiteCommand.Notify();
+                _newSiteUsername = value;
+            }
+        }
+        private string _newSitePassword;
+        public string NewSitePassword
+        {
+            get { return _newSitePassword; }
+            set
+            {
+                NewSiteCommand.Notify();
+                _newSitePassword = value;
+            }
+        }
+        private string _newSiteUrl;
+        public string NewSiteUrl
+        {
+            get { return _newSiteUrl; }
+            set
+            {
+                NewSiteCommand.Notify();
+                _newSiteUrl = value;
             }
         }
     }
